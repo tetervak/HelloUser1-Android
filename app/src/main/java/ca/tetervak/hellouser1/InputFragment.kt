91094454af01ccf0638260338fc21070b6e1fdd0
Alex Tetervak <tetervak@sheridancollege.ca>
2021-01-25 17:19:43 -0500

    switched to data binding

--- app/src/main/java/ca/tetervak/hellouser1/InputFragment.kt
+++ app/src/main/java/ca/tetervak/hellouser1/InputFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ca.tetervak.hellouser1.databinding.FragmentInputBinding
 
 class InputFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false)
+        val binding = FragmentInputBinding.inflate(layoutInflater, container,false)
+
+        return binding.root
     }
 
 }
--- app/src/main/java/ca/tetervak/hellouser1/OutputFragment.kt
+++ app/src/main/java/ca/tetervak/hellouser1/OutputFragment.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ca.tetervak.hellouser1.databinding.FragmentOutputBinding
 
 class OutputFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_output, container, false)
+        val binding = FragmentOutputBinding.inflate(inflater,container,false)
+
+
+
+        return binding.root
     }
 
 }
